2dbf7f7543dfb16cb3eea5c58b4118cdad63dfc7
PrathmeshFutane <63443691+PrathmeshFutane@users.noreply.github.com>
2021-03-23 16:17:39 +0530

    this is my first commit from ecllipse

---
+++ project-for-github/src/index.java
@@ -0,0 +1,12 @@
+
+public class index {
+	public static void main(String[] args) {
+		methone();
+	}
+	public static void methone(){
+		System.out.println("this is wonderful");
+	}
+	public static void methtwo() {
+		System.out.println("this is method two");
+	}
+}
